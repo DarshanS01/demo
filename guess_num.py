0133a6f026310d5ad77f6c4fcfc60d12fbf82113
Darshan <darshandarshu7520@gmail.com>
2025-06-21 23:16:36 +0530

    guess the currect number

---
+++ guess_num.py
@@ -0,0 +1,12 @@
+import random
+n=random.randint(1,101)
+a=-1
+guess=0
+while (a!=n):
+    guess+=1
+    a=int(input("Guess the number"))
+    if(a<n):
+        print("give hegher number")
+    elif(a>n):
+        print("give lower number")
+print(f"you guessed currect number{n}at guess{guess}")
